834f91283cd2bf4f341c35ea11459a7aa2a59c70
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-05 20:04:09 -0300

    Add files via upload

---
+++ script analisador de texto.py
@@ -0,0 +1,4 @@
+print('<<< OLa, vmaos fazer um analisador de texto >>>')
+texto = input('Digite algo: ')
+print('O tipo primitivo é:', type(texto))
+print('A quantidade de caracteres é:' ,len(texto))
---
+++ script para converte medidas.py
@@ -0,0 +1,5 @@
+print('ola, vamos fazer um converte metreos em: centimetros e milimetros')
+valorI = int(input('Digite o valor em METROS: '))
+cen = valorI * 100
+mil = valorI * 1000
+print('O valor em METROS foi: {}M \nEm CENTIMETROS foi: {}CM \nEm MILIMETROS foi: {}MM'.format(valorI, cen, mil))
