6871f02041facbd224f8ef7182431cc57dbbd0fc
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-09 20:37:47 -0300

    Add files via upload

---
+++ idade_em_dias.py
@@ -0,0 +1,4 @@
+nome = input('Olá, digite seu nome: ')
+idade = int(input('Olá, me diga sua idade: '))
+diasVividos = idade * 365
+print('Olá {}, você ja viveu aproximadamente: {} dias'.format(nome, diasVividos))
