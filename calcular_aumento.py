48ecddf237fc11a97415ca81052e35c3842f0e12
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-19 19:38:24 -0300

    Update and rename calcular_desconto.py to calcular_aumento.py
    correção de reajuste de desconto para aumento

---
+++ calcular_aumento.py
@@ -0,0 +1,3 @@
+salario = float(input('Digite seu salario: R$'))
+aumento = salario + (15 * salario ) / 100
+print('Seu salario total é de R${} \nE, após o aumneto ficará: R${}'.format(salario, aumento))
--- calcular_desconto.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Digite seu salario: R$'))
-desconto = salario - (15 * salario ) / 100
-print('Seu salario total é de R${} \nE após o desconto ficará: R${}'.format(salario, desconto))
