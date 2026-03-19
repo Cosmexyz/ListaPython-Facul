366c528bc3bd541afc07f6c973e3ebd4f8eeb192
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-19 19:41:23 -0300

    Fix output message and comments in desconto script

--- script para desconto.py
+++ script para desconto.py
@@ -1,6 +1,6 @@
-print('<<< ola, vamos calcular o seu descontro >>>')
-valorI = float(input('Digite o valor do seu produto: '))
+print('<<< ola, vamos calcular o desconto do seu produto >>>')
+valorI = float(input('Digite o valor do seu produto: ')) #I = inicial
 descontoI = int(input('Dgite o valordo seu desconto: '))
-descontoF = (valorI * descontoI) / 100
+descontoF = (valorI * descontoI) / 100 #F = Final
 valorF = valorI - descontoF
 print('O valor inicial foi de: R${} \nO valor do desconto foi de: R${}% \nE o valor a ser pago foi de: R${}'.format(valorI, descontoI, valorF))
