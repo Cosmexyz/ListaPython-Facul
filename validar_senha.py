8c9a0c3d5de2f91ac07dfe82db987fcd3c262185
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-19 20:50:55 -0300

    Add files via upload

---
+++ validar_senha.py
@@ -0,0 +1,7 @@
+while True:
+    senha = input('Digite a sua senha: ')
+    if senha == 'python123':
+        print('Acesso permitido.')
+        break
+    else:
+        print('Acesso negado, tente novamente.')
