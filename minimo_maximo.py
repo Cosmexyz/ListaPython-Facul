abf2a39f156c43bb2ab615bf17392961cc8e5b3c
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-19 19:25:45 -0300

    Add files via upload

---
+++ minimo_maximo.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite o PRIMERO número: '))
+num2 = int(input('Digite o SEGUNDO número: '))
+num3 = int(input('Digite o TERCEIRO número: '))
+
+numeros = [num1, num2, num3]
+
+print('O número max é:',max(numeros))
+print('O número min é:',min(numeros))
